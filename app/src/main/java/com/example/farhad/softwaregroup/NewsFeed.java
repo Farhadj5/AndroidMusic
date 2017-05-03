75138806d6b9b8ad21ac1fb42594df67d0c2e514
Bijan <wzj875@my.utsa.edu>
2017-05-02 21:30:07 -0500

    Changed Rssfeeder to a new twitter account

--- app/src/main/java/com/example/farhad/softwaregroup/NewsFeed.java
+++ app/src/main/java/com/example/farhad/softwaregroup/NewsFeed.java
@@ -5,6 +5,7 @@ package com.example.farhad.softwaregroup;
  */
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
 
@@ -14,6 +15,11 @@ public class NewsFeed extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newsfeed);
         WebView w1=(WebView)findViewById(R.id.webView);
-        w1.loadUrl("https://twitter.com/phoenixradio");
+        w1.loadUrl("https://twitter.com/phoenixradioxxx");
+    }
+    //What to do when the back button is pressed. Sends to the main screen
+    public void onBackPressed(){
+        Intent prevActivityIntent = new Intent(NewsFeed.this, MainActivity.class);
+        startActivity(prevActivityIntent);
     }
 }
